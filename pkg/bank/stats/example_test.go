6d04673e4da0a9f371d6f610c2975cd67bf3992b
Sami <dima-5050@mail.ru>
2021-10-10 01:42:59 +0500

    исправление ошибки в автотесте

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -14,15 +14,15 @@ func ExampleAvg() {
 		},
 		{
 			ID: 2,
-			Amount: 3_000_00,
+			Amount: 2_000_00,
 		},
 		{
 			ID: 3,
-			Amount: 4_000_00,
+			Amount: 3_000_00,
 		},
 	}
 
 	fmt.Println(Avg(payment))
-	//Output: 400000
+	//Output: 200000
 	
 }
