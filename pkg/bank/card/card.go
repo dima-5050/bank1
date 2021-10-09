7a33a6b1c71cc93ca1ecd9b34019c335baf357f6
Sami <dima-5050@mail.ru>
2021-10-09 23:19:43 +0500

    добавлена функции Withdraw

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,26 @@
+package card
+
+import (
+	"github.com/dima-5050/bank/pkg/bank/types"
+)
+
+func Withdraw(card *types.Card, amount types.Money) {
+	const withdrawLimit = 20_000_00
+	if amount < 0 {
+		return
+	}
+
+	if amount > withdrawLimit {
+		return
+	}
+
+	if !card.Active {
+		return
+	}
+
+	if card.Balance < amount {
+		return
+	}
+
+	card.Balance-=amount
+}
